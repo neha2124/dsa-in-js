e86f86167a5b020150f6e5a9190bdf70d791a531
neha2124 <nehashukla2124@gmail.com>
2023-02-27 14:40:00 +0530

    first commit

---
+++ array trversing/script.js
@@ -0,0 +1,31 @@
+let arr = [1,23,34,33,4,45,78,9,01,10,39,38]
+// traverse in array
+// for(let i=0; i<arr.length; i++){
+//     document.write(` arr of ${i} is ${arr[i]} <br>`)
+// }
+//accessing the given value
+let el = document.getElementById("element")
+function get(){
+   let ele = el.value
+    if(ele < arr.length ){
+     alert(arr[ele])
+    }else{
+        alert("please input valid value")
+    }
+}
+// inserting a value at given position 
+let data = [1,23,33,23,4,5,9,0,29,]
+function insert (){
+    let pos = document.getElementById("position").value;
+    let newEl = document.getElementById("new-el").value
+    newEl = parseInt(newEl)
+    console.log(data)
+    for(let i= data.length-1; i>=0; i--){
+        if(i == pos){
+            data[i] =newEl
+        }
+    }
+console.log(data)
+}
+// insert()
+
